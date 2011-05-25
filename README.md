e59ac9bd15f2a17101a59eba1518122b00562be3
Fredrick Galoso <fred@wayoutmind.com>
2011-05-25 13:20:44 -0500

    init Gauss

---
+++ README.md
@@ -0,0 +1,169 @@
+#Gauss
+> JavaScript statistics and analytics library - [Node.JS](http://nodejs.org/) ready
+
+Evented and asynchronous from the bottom up, [Node.JS][1] is an attractive platform for data mining, statistics, and data analysis.
+[Gauss](http://en.wikipedia.org/wiki/Carl_Friedrich_Gauss) makes it easy to calculate and explore data through JavaScript.
+
+[1]: http://nodejs.org/
+
+##License
+MIT/X11 - See [LICENSE][2]
+
+[2]: http://github.com/stackd/gauss/blob/master/LICENSE
+
+##Getting started
+
+###Install with NPM (Node Package Manager)
+
+Getting started with Gauss + Node.JS is easy:
+
+    $ npm install gauss
+
+###Build for browser/embedded applications
+
+Generates a minified gauss.js (Requires Java Runtime Environment to run Google's [Closure Compiler](http://code.google.com/closure/compiler/))
+
+    $ git clone git://github.com/stackd/gauss.git
+    $ cd gauss
+    $ make
+
+###Installing development dependencies and running tests
+
+To run Gauss's tests you'll need [Vows](http://vowsjs.org/). NPM can automatically resolve this:
+
+    $ npm install gauss --devel
+
+You can then invoke the tests with either `node` or `vows`:
+
+*Simple test run*
+
+    $ node test/*
+
+*Run tests and view specification (verbose)*
+
+    $ vows test/* --spec
+    
+##API
+
+For now Gauss only has methods for univariate analysis. We're constantly working on adding more functions, adding multivariate statistics, and we encourage additions to the library. Accuracy is a primary concern. If Gauss is returning incorrect results, [please submit an issue](https://github.com/stackd/gauss/issues) and/or [submit a patch](https://github.com/stackd/gauss#fork_box)!
+
+###Instantiation
+
+Conventional:
+
+    var gauss = require('gauss');
+    var set = new guass.Vector(5, 1, 3, 2, 21);
+
+Monkey patch:
+
+    Array = require('gauss').Vector;
+    var set = [5, 1, 3, 2, 21];
+
+###Callbacks and method chaining
+
+All of Gauss's methods accept an *optional* [callback][3]:
+
+[3]: http://en.wikipedia.org/wiki/Callback_(computer_programming)
+
+    set.min();
+    set.min(function(result) {
+        result / 2;
+        /* Do more things with the minimum*/
+    });
+
+In addition, for methods that return another Vector, method chaining makes it easy to perform calculations that flow through each other:
+
+    set.quantile(4).stdev(); // Find the standard deviation of data set's quartiles
+
+Finally, you can mix and match both callbacks and chaining:
+
+    set.quantile(4).stdev(function(stdev) {
+        if (stdev > 1) {
+            /* Do something awesome */
+        }
+    });
+
+###Methods
+
+**.min(callback)**
+
+Returns the smallest number.
+
+**.max(callback)**
+
+Returns the largest number.
+
+**.sum(callback)**
+
+Returns the sum of the numbers.
+
+**.range(callback)**
+
+Returns the difference between the largest and smallest value in a data set.
+
+**.mean(callback)**
+
+Returns the average.
+
+**.median(callback)**
+
+Returns the median. If there are an even amount of numbers in the data set, returns the average of the two middle values.
+
+**.mode(callback)**
+
+Returns the value(s) that occur the most frequently in a data set. If there is a tie, returns an array of values.
+
+**.variance(callback)**
+
+Returns a measure of how far a set of numbers are spread out from each other.
+
+**.stdev(percent, callback)**
+
+Returns the standard deviation of data set. If a percent is given, returns the standard deviation with respect to a percentile of the population.
+
+**.frequency(value, callback)**
+
+Returns the number of occurrences of value within a data set.
+
+**.percentile(value, callback)**
+
+Returns the value that below which a certain percent of observations fall within the data set.
+
+**.density(percent, callback)**
+
+Returns an array which is a percentile subset of values occurring within a data set.
+
+**.distribution(format, callback)**
+
+Returns an `Object` containing the (frequency) distribution of values within the array. Default format: `absolute`; `relative` returns ratio of occurrences and total number of values in a data set. 
+
+    set.distribution();
+    > {
+        1: 1,
+        2: 1,
+        3: 1,
+        5: 1,
+        21: 1
+      }
+    set.distribution('relative');
+    > {
+        1: 0.2,
+        2: 0.2,
+        3: 0.2,
+        5: 0.2,
+        21: 0.2
+      }
+
+**.quantile(quantity, callback)**
+
+Returns an array of values that divide a frequency distribution into equal groups, each containing the same fraction of the total data set.
+
+    set.quantile(4); // Quartiles
+
+**.copy(callback)**
+
+Returns a copy of the data set.
+
+**.clone(callback)**
+
+Returns another instance of the Vector object and data.
---
+++ test/vector.test.js
@@ -0,0 +1,190 @@
+/**
+ * Test Vector data structure and methods.
+ */
+
+var vows = require('vows'),
+    assert = require('assert');
+
+/**
+ * Let's non-invasivly duck punch and extend
+ * the Array type with some of gauss's goodies.
+ */
+
+Array = require('../lib/vector').Vector;
+
+var set = [10, 82, 67, 17, 36, 3, 1, 61, 33, 20,
+18, 35, 15, 39, 52, 85, 17, 92, 88, 70,
+66, 85, 93, 81, 70, 41, 40, 22, 38, 52,
+86, 60, 64, 38, 87, 15, 92, 61, 93, 17,
+38, 68, 11, 98, 62, 75, 94, 63, 49, 97];
+
+var majority = [10, 82, 67, 17, 36, 3, 1, 61, 33, 20,
+18, 35, 15, 39, 52, 85, 17, 92, 88, 70,
+66, 85, 93, 81, 70, 41, 40, 22, 38, 52,
+86, 60, 64, 38, 87, 15, 92, 61, 93, 17,
+38, 68, 11, 98, 62, 75, 94, 63, 17];
+
+vows.describe('Vector').addBatch({
+    'Minimum': {
+        topic: set.min(),
+        '1': function(topic) {
+            assert.equal(topic, 1);
+        }
+    },
+    'Maximum': {
+        topic: set.max(),
+        '98': function(topic) {
+            assert.equal(topic, 98);
+        }
+    },
+    'Sum': {
+        topic: set.sum(),
+        '2697': function(topic) {
+            assert.equal(topic, 2697);
+        }
+    },
+    'Mean': {
+        topic: set.mean(),
+        '53.94': function(topic) {
+            assert.equal(topic, 53.94);
+        } 
+    },
+    'Median': {
+        '(Even number of elements)': {
+            topic: set.median(),
+            '60.5, mean of the two middle numbers':
+                function(topic) {
+                    assert.equal(topic, 60.5);
+                }
+        },
+        '(Odd number of elements)': {
+            topic: function() {
+                var odd = set.copy();
+                odd.pop();
+                return odd;
+            },
+            '60': function(topic) {
+                assert.equal(topic.median(), 60);
+            }
+        }
+    },
+    'Mode': {
+        '(Tie)': {
+            topic: set.mode(),
+            '[17, 38]': function(topic) {
+                assert.deepEqual(topic, [17, 38]);
+            }
+        },
+        '(Majority)': {
+            topic: majority.mode(),
+            '17': function(topic) {
+                    assert.equal(topic, 17);
+            }
+        }
+    },
+    'Range': {
+        topic: set.range(),
+        '97': function(topic) {
+            assert.equal(topic, 97);
+        }  
+    },
+    'Variance': {
+        topic: set.variance(),
+        '843.9364': function(topic) {
+            assert.equal(topic, 843.9364);
+        }
+    },
+    'Standard Deviation': {
+        '(Population)': {
+            topic: set.stdev(),
+            '29.050583470904677': function(topic) {
+                assert.equal(topic, 29.050583470904677);
+            }
+        },
+        '(Percentage - 95%)': {
+            topic: set.stdev(.95),
+            '28.226483574827384': function(topic) {
+                assert.equal(topic, 28.226483574827384);
+            }
+        }
+    },
+    'Frequency': {
+        topic: set.frequency(17),
+        '3': function(topic) {
+            assert.equal(topic, 3);
+        }
+    },
+    'Percentile': {
+        topic: set.percentile(.90),
+        '92': function(topic) {
+            assert.equal(topic, 92);
+        }
+    },
+    'Density': {
+        topic: set.density(.25),
+        'Array of values (25% density)': function(topic) {
+            assert.deepEqual(topic, [39, 40, 41, 49, 52, 52, 60, 61, 61, 62, 63, 64]);
+        }
+    },
+    'Distribution': {
+        '(Absolute Frequency)': {
+            topic: set.distribution(),
+            '{Element: Count}': function(topic) {
+                assert.deepEqual(topic, {
+                    1: 1, 3: 1, 10: 1, 11: 1, 15: 2,
+                    17: 3, 18: 1, 20: 1, 22: 1, 33: 1,
+                    35: 1, 36: 1, 38: 3, 39: 1, 40: 1,
+                    41: 1, 49: 1, 52: 2, 60: 1, 61: 2,
+                    62: 1, 63: 1, 64: 1, 66: 1, 67: 1,
+                    68: 1, 70: 2, 75: 1, 81: 1, 82: 1,
+                    85: 2, 86: 1, 87: 1, 88: 1, 92: 2,
+                    93: 2, 94: 1, 97: 1, 98: 1
+                });
+            }
+        },
+        '(Relative Frequency)': {
+            topic: set.distribution('relative'),
+            '{Element: Ratio}': function(topic) {
+                assert.deepEqual(topic, {
+                    1: 0.02, 3: 0.02, 10: 0.02, 11: 0.02, 15: 0.04,
+                    17: 0.06, 18: 0.02, 20: 0.02, 22: 0.02, 33: 0.02,
+                    35: 0.02, 36: 0.02, 38: 0.06, 39: 0.02, 40: 0.02,
+                    41: 0.02, 49: 0.02, 52: 0.04, 60: 0.02, 61: 0.04,
+                    62: 0.02, 63: 0.02, 64: 0.02, 66: 0.02, 67: 0.02,
+                    68: 0.02, 70: 0.04, 75: 0.02, 81: 0.02, 82: 0.02,
+                    85: 0.04, 86: 0.02, 87: 0.02, 88: 0.02, 92: 0.04,
+                    93: 0.04, 94: 0.02, 97: 0.02, 98: 0.02
+                });
+            }
+        }
+    },
+    'Quantile': {
+        '(Quartile: 4-quantiles)': {
+            topic: set.quantile(4),
+            '[ 33, 60, 82 ]': function(topic) {
+                assert.deepEqual(topic, [ 33, 60, 82 ]);
+            }
+        },
+        '(Decile: 10-quantiles)': {
+            topic: set.quantile(10),
+            '[15, 18, 36, 40, 60, 64, 70, 85, 92]': function(topic) {
+                assert.deepEqual(topic, [
+                    15, 18, 36, 40, 60,
+                    64, 70, 85, 92
+                ]);
+            }
+        }
+    },
+    'Copy': {
+        topic: set.copy(),
+        'Values === Parent Vector': function(topic) {
+            assert.deepEqual(topic, set);
+        }
+    },
+    'Clone': {
+        topic: set.clone(),
+        'Instance of Parent Vector': function(topic) {
+            assert.instanceOf(topic, Array);
+        }
+    }
+}).export(module);
