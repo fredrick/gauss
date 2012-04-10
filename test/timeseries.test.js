e3f5989bde95b5ecb23203e34f6c65073af62424
Fredrick Galoso <fred@wayoutmind.com>
2012-04-09 20:50:33 -0500

    4 space tab to 2 space tab conversion

--- test/timeseries.test.js
+++ test/timeseries.test.js
@@ -10,16 +10,16 @@ var TimeSeries = require('../lib/gauss').TimeSeries;
 var series = new TimeSeries([1315378833000, 2], [1315789015000, 4]);
 
 vows.describe('TimeSeries').addBatch({
-    'Times': {
-        topic: series.times(),
-        '[1315378833000, 1315789015000]': function(topic) {
-            assert.deepEqual(topic, [1315378833000, 1315789015000]);
-        }
-    },
-    'Values': {
-    	topic: series.values(),
-    	'[2, 4]': function(topic) {
-    		assert.deepEqual(topic, [2, 4]);
-    	}
+  'Times': {
+    topic: series.times(),
+    '[1315378833000, 1315789015000]': function(topic) {
+      assert.deepEqual(topic, [1315378833000, 1315789015000]);
     }
+  },
+  'Values': {
+  	topic: series.values(),
+  	'[2, 4]': function(topic) {
+  		assert.deepEqual(topic, [2, 4]);
+  	}
+  }
 }).export(module);
--- test/vector.test.js
+++ test/vector.test.js
@@ -32,278 +32,278 @@ var prices = new Vector([22.2734, 22.194, 22.0847, 22.1741, 22.184, 22.1344,
 23.0976, 23.326, 22.6805, 23.0976, 22.4025, 22.1725]);
 
 vows.describe('Vector').addBatch({
-    'Minimum': {
-        topic: set.min(),
-        '1': function(topic) {
-            assert.equal(topic, 1);
-        }
-    },
-    'Maximum': {
-        topic: set.max(),
-        '98': function(topic) {
-            assert.equal(topic, 98);
-        }
-    },
-    'Sum': {
-        topic: set.sum(),
-        '2697': function(topic) {
-            assert.equal(topic, 2697);
-        }
-    },
-    'Product': {
-        topic: set.product(),
-        '7.120776302117291e+80': function(topic) {
-            assert.equal(topic, 7.120776302117291e+80);
-        }
-    },
-    'Arithmetic Mean': {
-        topic: set.mean(),
-        '53.94': function(topic) {
-            assert.equal(topic, 53.94);
-        } 
-    },
-    'Geometric Mean': {
-        topic: set.gmean(),
-        '53.94': function(topic) {
-            assert.equal(topic, 41.40478623971778);
-        } 
-    },
-    'Harmonic Mean': {
-        topic: set.hmean(),
-        '53.94': function(topic) {
-            assert.equal(topic, 19.0456068931919);
-        } 
-    },
-    'Quadratic Mean': {
-        topic: set.qmean(),
-        '61.26548783777046': function(topic) {
-            assert.equal(topic, 61.26548783777046);
-        }  
-    },
-    'Median': {
-        '(Even number of elements)': {
-            topic: set.median(),
-            '60.5, mean of the two middle numbers':
-                function(topic) {
-                    assert.equal(topic, 60.5);
-                }
-        },
-        '(Odd number of elements)': {
-            topic: function() {
-                var odd = set.copy();
-                odd.pop();
-                return odd;
-            },
-            '60': function(topic) {
-                assert.equal(topic.median(), 60);
-            }
-        }
-    },
-    'Mode': {
-        '(Tie)': {
-            topic: set.mode().toArray(),
-            '[17, 38]': function(topic) {
-                assert.deepEqual(topic, [17, 38]);
-            }
-        },
-        '(Majority)': {
-            topic: majority.mode(),
-            '17': function(topic) {
-                    assert.equal(topic, 17);
-            }
-        },
-        '(Uniform)': {
-            topic: new Vector([1, 2, 3, 4]).mode().toArray(),
-            '[1, 2, 3, 4]': function(topic) {
-                assert.deepEqual(topic, [1, 2, 3, 4]);
-            }
-        }
-    },
-    'Range': {
-        topic: set.range(),
-        '97': function(topic) {
-            assert.equal(topic, 97);
-        }  
-    },
-    'Variance': {
-        topic: set.variance(),
-        '843.9364': function(topic) {
-            assert.equal(topic, 843.9364);
-        }
-    },
-    'Standard Deviation': {
-        '(Population)': {
-            topic: set.stdev(),
-            '29.050583470904677': function(topic) {
-                assert.equal(topic, 29.050583470904677);
-            }
-        },
-        '(Percentage - 95%)': {
-            topic: set.stdev(.95),
-            '28.226483574827384': function(topic) {
-                assert.equal(topic, 28.226483574827384);
-            }
-        }
-    },
-    'Frequency': {
-        topic: set.frequency(17),
-        '3': function(topic) {
-            assert.equal(topic, 3);
-        }
-    },
-    'Percentile': {
-        topic: set.percentile(.90),
-        '93': function(topic) {
-            assert.equal(topic, 93);
-        }
-    },
-    'Density': {
-        topic: set.density(.25).toArray(),
-        'Array of values (25% density)': function(topic) {
-            assert.deepEqual(topic, [39, 40, 41, 49, 52, 52, 60, 61, 61, 62, 63, 64]);
-        }
-    },
-    'Distribution': {
-        '(Absolute Frequency)': {
-            topic: set.distribution(),
-            '{Element: Count}': function(topic) {
-                assert.deepEqual(topic, {
-                    1: 1, 3: 1, 10: 1, 11: 1, 15: 2,
-                    17: 3, 18: 1, 20: 1, 22: 1, 33: 1,
-                    35: 1, 36: 1, 38: 3, 39: 1, 40: 1,
-                    41: 1, 49: 1, 52: 2, 60: 1, 61: 2,
-                    62: 1, 63: 1, 64: 1, 66: 1, 67: 1,
-                    68: 1, 70: 2, 75: 1, 81: 1, 82: 1,
-                    85: 2, 86: 1, 87: 1, 88: 1, 92: 2,
-                    93: 2, 94: 1, 97: 1, 98: 1
-                });
-            }
-        },
-        '(Relative Frequency)': {
-            topic: set.distribution('relative'),
-            '{Element: Ratio}': function(topic) {
-                assert.deepEqual(topic, {
-                    1: 0.02, 3: 0.02, 10: 0.02, 11: 0.02, 15: 0.04,
-                    17: 0.06, 18: 0.02, 20: 0.02, 22: 0.02, 33: 0.02,
-                    35: 0.02, 36: 0.02, 38: 0.06, 39: 0.02, 40: 0.02,
-                    41: 0.02, 49: 0.02, 52: 0.04, 60: 0.02, 61: 0.04,
-                    62: 0.02, 63: 0.02, 64: 0.02, 66: 0.02, 67: 0.02,
-                    68: 0.02, 70: 0.04, 75: 0.02, 81: 0.02, 82: 0.02,
-                    85: 0.04, 86: 0.02, 87: 0.02, 88: 0.02, 92: 0.04,
-                    93: 0.04, 94: 0.02, 97: 0.02, 98: 0.02
-                });
-            }
-        }
-    },
-    'Quantile': {
-        '(Quartile: 4-quantiles)': {
-            topic: set.quantile(4).toArray(),
-            '[ 33, 60, 82 ]': function(topic) {
-                assert.deepEqual(topic, [ 33, 60, 82 ]);
-            }
-        },
-        '(Decile: 10-quantiles)': {
-            topic: set.quantile(10).toArray(),
-            '[15, 18, 36, 40, 60, 64, 70, 85, 92]': function(topic) {
-                assert.deepEqual(topic, [
-                    15, 18, 36, 40, 60,
-                    64, 70, 85, 92
-                ]);
-            }
-        }
-    },
-    'Delta': {
-        topic: set.delta().toArray(),
-        'Sequential differences': function(topic) {
-            assert.deepEqual(topic, [
-                72, -15, -50, 19, -33, -2, 60, -28, -13, -2,
-                17, -20, 24, 13, 33, -68, 75, -4, -18, -4,
-                19, 8, -12, -11, -29, -1, -18, 16, 14, 34,
-                -26, 4, -26, 49, -72, 77, -31, 32, -76, 21,
-                30, -57, 87, -36, 13, 19, -31, -14, 48
-            ]);
-        }
-    },
-    'Simple Moving Average': {
-        '(10)': {
-            topic: prices.sma(10).toArray(),
-            '10-period SMA': function(topic) {
-                assert.deepEqual(topic, [
-                    22.22475, 22.21283, 22.232689999999998,
-                    22.26238, 22.30606, 22.42324,
-                    22.61499, 22.76692, 22.90693,
-                    23.07773, 23.211779999999997, 23.37861,
-                    23.52657, 23.653779999999998, 23.711389999999998,
-                    23.68557, 23.61298, 23.50573,
-                    23.43225, 23.27734, 23.13121
-                ]);
-            }
-        },
-        '(1)': {
-            topic: prices.sma(1),
-            'Single-period SMA': function(topic) {
-                assert.deepEqual(topic, prices);
-            }
-        }
-    },
-    'Exponential Moving Average': {
-        '(1)': {
-            topic: prices.ema(1).toArray(),
-            'Single-period EMA': function(topic) {
-                assert.deepEqual(topic, prices.toArray());
-            }
-        },
-        '(10)': {
-            topic: prices.ema(10).toArray(),
-            '10-period EMA': function(topic) {
-                assert.deepEqual(topic, [
-                    22.22475, 22.21192272727273, 22.24477314049587,
-                    22.269650751314803, 22.331696069257568, 22.51789678393801,
-                    22.796806459585646, 22.970659830570074, 23.127339861375514,
-                    23.27720534112542, 23.34204073364807, 23.429396963893875,
-                    23.509906606822263, 23.536050860127308, 23.47258706737689,
-                    23.40440760058109, 23.390151673202713, 23.261124096256765,
-                    23.231392442391897, 23.080684725593372, 22.91556023003094
-                ]);
-            }
-        },
-        '(10 Wilder)': {
-            topic: prices.ema({ period: 10, ratio: function(n) { return 1 / n; } }).toArray(),
-            '10-period Welles Wilder EMA': function(topic) {
-                assert.deepEqual(topic, [
-                    22.22475, 22.217695, 22.2351855,
-                    22.24982695, 22.285934255, 22.3929208295,
-                    22.55881874655, 22.678236871895, 22.793653184705498,
-                    22.90944786623495, 22.981883079611453, 23.065944771650308,
-                    23.146570294485276, 23.19728326503675, 23.196254938533073,
-                    23.186389444679765, 23.20035050021179, 23.14836545019061,
-                    23.14328890517155, 23.069210014654395, 22.979539013188955
-                ]);
-            }
-        }
-    },
-    'Absolute Value': {
-        topic: heterogeneous.abs().toArray(),
-        'Modulus': function(topic) {
-            assert.deepEqual(topic, [
-                72, 15, 50, 19, 33, 2, 60, 28, 13, 2,
-                17, 20, 24, 13, 33, 68, 75, 4, 18, 4,
-                19, 8, 12, 11, 29, 1, 18, 16, 14, 34,
-                26, 4, 26, 49, 72, 77, 31, 32, 76, 21,
-                30, 57, 87, 36, 13, 19, 31, 14, 48
-            ]);
-        }
-    },
-    'Copy': {
-        topic: set.copy().toArray(),
-        'Values === Parent Vector': function(topic) {
-            assert.deepEqual(topic, set.toArray());
-        }
-    },
-    'Clone': {
-        topic: set.clone(),
-        'Instance of Parent Vector': function(topic) {
-            assert.instanceOf(topic, Array);
-        }
+  'Minimum': {
+    topic: set.min(),
+    '1': function(topic) {
+      assert.equal(topic, 1);
     }
+  },
+  'Maximum': {
+    topic: set.max(),
+    '98': function(topic) {
+      assert.equal(topic, 98);
+    }
+  },
+  'Sum': {
+  topic: set.sum(),
+    '2697': function(topic) {
+      assert.equal(topic, 2697);
+    }
+  },
+  'Product': {
+    topic: set.product(),
+    '7.120776302117291e+80': function(topic) {
+      assert.equal(topic, 7.120776302117291e+80);
+    }
+  },
+  'Arithmetic Mean': {
+    topic: set.mean(),
+    '53.94': function(topic) {
+      assert.equal(topic, 53.94);
+    }
+  },
+  'Geometric Mean': {
+    topic: set.gmean(),
+    '53.94': function(topic) {
+      assert.equal(topic, 41.40478623971778);
+    } 
+  },
+  'Harmonic Mean': {
+    topic: set.hmean(),
+    '53.94': function(topic) {
+      assert.equal(topic, 19.0456068931919);
+    } 
+  },
+  'Quadratic Mean': {
+    topic: set.qmean(),
+    '61.26548783777046': function(topic) {
+      assert.equal(topic, 61.26548783777046);
+    }  
+  },
+  'Median': {
+    '(Even number of elements)': {
+      topic: set.median(),
+      '60.5, mean of the two middle numbers':
+        function(topic) {
+          assert.equal(topic, 60.5);
+        }
+    },
+    '(Odd number of elements)': {
+      topic: function() {
+        var odd = set.copy();
+        odd.pop();
+        return odd;
+      },
+      '60': function(topic) {
+        assert.equal(topic.median(), 60);
+      }
+    }
+  },
+  'Mode': {
+    '(Tie)': {
+      topic: set.mode().toArray(),
+      '[17, 38]': function(topic) {
+        assert.deepEqual(topic, [17, 38]);
+      }
+    },
+    '(Majority)': {
+      topic: majority.mode(),
+      '17': function(topic) {
+          assert.equal(topic, 17);
+      }
+    },
+    '(Uniform)': {
+      topic: new Vector([1, 2, 3, 4]).mode().toArray(),
+      '[1, 2, 3, 4]': function(topic) {
+        assert.deepEqual(topic, [1, 2, 3, 4]);
+      }
+    }
+  },
+  'Range': {
+    topic: set.range(),
+    '97': function(topic) {
+      assert.equal(topic, 97);
+    }  
+  },
+  'Variance': {
+    topic: set.variance(),
+    '843.9364': function(topic) {
+      assert.equal(topic, 843.9364);
+    }
+  },
+  'Standard Deviation': {
+    '(Population)': {
+      topic: set.stdev(),
+      '29.050583470904677': function(topic) {
+        assert.equal(topic, 29.050583470904677);
+      }
+    },
+    '(Percentage - 95%)': {
+      topic: set.stdev(.95),
+      '28.226483574827384': function(topic) {
+        assert.equal(topic, 28.226483574827384);
+      }
+    }
+  },
+  'Frequency': {
+    topic: set.frequency(17),
+    '3': function(topic) {
+      assert.equal(topic, 3);
+    }
+  },
+  'Percentile': {
+    topic: set.percentile(.90),
+    '93': function(topic) {
+      assert.equal(topic, 93);
+    }
+  },
+  'Density': {
+    topic: set.density(.25).toArray(),
+    'Array of values (25% density)': function(topic) {
+      assert.deepEqual(topic, [39, 40, 41, 49, 52, 52, 60, 61, 61, 62, 63, 64]);
+    }
+  },
+  'Distribution': {
+    '(Absolute Frequency)': {
+      topic: set.distribution(),
+      '{Element: Count}': function(topic) {
+        assert.deepEqual(topic, {
+          1: 1, 3: 1, 10: 1, 11: 1, 15: 2,
+          17: 3, 18: 1, 20: 1, 22: 1, 33: 1,
+          35: 1, 36: 1, 38: 3, 39: 1, 40: 1,
+          41: 1, 49: 1, 52: 2, 60: 1, 61: 2,
+          62: 1, 63: 1, 64: 1, 66: 1, 67: 1,
+          68: 1, 70: 2, 75: 1, 81: 1, 82: 1,
+          85: 2, 86: 1, 87: 1, 88: 1, 92: 2,
+          93: 2, 94: 1, 97: 1, 98: 1
+        });
+      }
+      },
+    '(Relative Frequency)': {
+      topic: set.distribution('relative'),
+      '{Element: Ratio}': function(topic) {
+        assert.deepEqual(topic, {
+          1: 0.02, 3: 0.02, 10: 0.02, 11: 0.02, 15: 0.04,
+          17: 0.06, 18: 0.02, 20: 0.02, 22: 0.02, 33: 0.02,
+          35: 0.02, 36: 0.02, 38: 0.06, 39: 0.02, 40: 0.02,
+          41: 0.02, 49: 0.02, 52: 0.04, 60: 0.02, 61: 0.04,
+          62: 0.02, 63: 0.02, 64: 0.02, 66: 0.02, 67: 0.02,
+          68: 0.02, 70: 0.04, 75: 0.02, 81: 0.02, 82: 0.02,
+          85: 0.04, 86: 0.02, 87: 0.02, 88: 0.02, 92: 0.04,
+          93: 0.04, 94: 0.02, 97: 0.02, 98: 0.02
+        });
+      }
+    }
+  },
+  'Quantile': {
+    '(Quartile: 4-quantiles)': {
+      topic: set.quantile(4).toArray(),
+      '[ 33, 60, 82 ]': function(topic) {
+        assert.deepEqual(topic, [ 33, 60, 82 ]);
+      }
+    },
+    '(Decile: 10-quantiles)': {
+      topic: set.quantile(10).toArray(),
+      '[15, 18, 36, 40, 60, 64, 70, 85, 92]': function(topic) {
+        assert.deepEqual(topic, [
+          15, 18, 36, 40, 60,
+          64, 70, 85, 92
+        ]);
+      }
+    }
+  },
+  'Delta': {
+    topic: set.delta().toArray(),
+    'Sequential differences': function(topic) {
+      assert.deepEqual(topic, [
+        72, -15, -50, 19, -33, -2, 60, -28, -13, -2,
+        17, -20, 24, 13, 33, -68, 75, -4, -18, -4,
+        19, 8, -12, -11, -29, -1, -18, 16, 14, 34,
+        -26, 4, -26, 49, -72, 77, -31, 32, -76, 21,
+        30, -57, 87, -36, 13, 19, -31, -14, 48
+      ]);
+    }
+  },
+  'Simple Moving Average': {
+    '(10)': {
+      topic: prices.sma(10).toArray(),
+      '10-period SMA': function(topic) {
+        assert.deepEqual(topic, [
+          22.22475, 22.21283, 22.232689999999998,
+          22.26238, 22.30606, 22.42324,
+          22.61499, 22.76692, 22.90693,
+          23.07773, 23.211779999999997, 23.37861,
+          23.52657, 23.653779999999998, 23.711389999999998,
+          23.68557, 23.61298, 23.50573,
+          23.43225, 23.27734, 23.13121
+        ]);
+      }
+    },
+    '(1)': {
+      topic: prices.sma(1),
+      'Single-period SMA': function(topic) {
+        assert.deepEqual(topic, prices);
+      }
+    }
+  },
+  'Exponential Moving Average': {
+    '(1)': {
+      topic: prices.ema(1).toArray(),
+      'Single-period EMA': function(topic) {
+        assert.deepEqual(topic, prices.toArray());
+      }
+    },
+    '(10)': {
+      topic: prices.ema(10).toArray(),
+      '10-period EMA': function(topic) {
+        assert.deepEqual(topic, [
+          22.22475, 22.21192272727273, 22.24477314049587,
+          22.269650751314803, 22.331696069257568, 22.51789678393801,
+          22.796806459585646, 22.970659830570074, 23.127339861375514,
+          23.27720534112542, 23.34204073364807, 23.429396963893875,
+          23.509906606822263, 23.536050860127308, 23.47258706737689,
+          23.40440760058109, 23.390151673202713, 23.261124096256765,
+          23.231392442391897, 23.080684725593372, 22.91556023003094
+        ]);
+      }
+    },
+    '(10 Wilder)': {
+      topic: prices.ema({ period: 10, ratio: function(n) { return 1 / n; } }).toArray(),
+      '10-period Welles Wilder EMA': function(topic) {
+        assert.deepEqual(topic, [
+          22.22475, 22.217695, 22.2351855,
+          22.24982695, 22.285934255, 22.3929208295,
+          22.55881874655, 22.678236871895, 22.793653184705498,
+          22.90944786623495, 22.981883079611453, 23.065944771650308,
+          23.146570294485276, 23.19728326503675, 23.196254938533073,
+          23.186389444679765, 23.20035050021179, 23.14836545019061,
+          23.14328890517155, 23.069210014654395, 22.979539013188955
+        ]);
+      }
+    }
+  },
+  'Absolute Value': {
+    topic: heterogeneous.abs().toArray(),
+    'Modulus': function(topic) {
+      assert.deepEqual(topic, [
+        72, 15, 50, 19, 33, 2, 60, 28, 13, 2,
+        17, 20, 24, 13, 33, 68, 75, 4, 18, 4,
+        19, 8, 12, 11, 29, 1, 18, 16, 14, 34,
+        26, 4, 26, 49, 72, 77, 31, 32, 76, 21,
+        30, 57, 87, 36, 13, 19, 31, 14, 48
+      ]);
+    }
+  },
+  'Copy': {
+    topic: set.copy().toArray(),
+    'Values === Parent Vector': function(topic) {
+      assert.deepEqual(topic, set.toArray());
+    }
+  },
+  'Clone': {
+    topic: set.clone(),
+    'Instance of Parent Vector': function(topic) {
+      assert.instanceOf(topic, Array);
+    }
+  }
 }).export(module);
