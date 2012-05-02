ef5a002d4634d6bf360837bf0e2e28b4e0c80713
Fredrick Galoso <fred@wayoutmind.com>
2012-05-02 15:58:15 -0500

    TimeSeries accessors return Vectors

--- test/timeseries.test.js
+++ test/timeseries.test.js
@@ -11,13 +11,13 @@ var series = new TimeSeries([1315378833000, 2], [1315789015000, 4]);
 
 vows.describe('TimeSeries').addBatch({
   'Times': {
-    topic: series.times(),
+    topic: series.times().toArray(),
     '[1315378833000, 1315789015000]': function(topic) {
       assert.deepEqual(topic, [1315378833000, 1315789015000]);
     }
   },
   'Values': {
-  	topic: series.values(),
+  	topic: series.values().toArray(),
   	'[2, 4]': function(topic) {
   		assert.deepEqual(topic, [2, 4]);
   	}
