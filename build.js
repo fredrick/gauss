04a40ae16a49d243cd1472e97b51a59026b1dc72
Fredrick Galoso <fred@wayoutmind.com>
2012-06-05 15:22:04 -0500

    Suppress copyrights when minifying

--- build.js
+++ build.js
@@ -3,7 +3,7 @@
 var child_process = require('child_process');
 
 (function(next) {
-  child_process.exec('node_modules/.bin/uglifyjs lib/vector.js > gauss.min.js', next);
+  child_process.exec('node_modules/.bin/uglifyjs -nc lib/vector.js > gauss.min.js', next);
 })(function() {
-  child_process.exec('node_modules/.bin/uglifyjs lib/timeseries.js >> gauss.min.js');
+  child_process.exec('node_modules/.bin/uglifyjs -nc lib/timeseries.js >> gauss.min.js');
 });
