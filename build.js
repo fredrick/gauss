422ebb41a3fe705767cd553fc20b0490f5656739
Fredrick Galoso <fred@wayoutmind.com>
2012-06-06 17:04:24 -0500

    Update build script to match child_process.exec format
    - Convention per
      http://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback

--- build.js
+++ build.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-var child_process = require('child_process');
+/**
+ * Build Gauss for web browser
+ */
+
+var exec = require('child_process').exec;
 
 (function(next) {
-  child_process.exec('node_modules/.bin/uglifyjs -nc lib/vector.js > gauss.min.js', next);
+  exec('node_modules/.bin/uglifyjs -nc lib/vector.js > gauss.min.js', next);
 })(function() {
-  child_process.exec('node_modules/.bin/uglifyjs -nc lib/timeseries.js >> gauss.min.js');
+  exec('node_modules/.bin/uglifyjs -nc lib/timeseries.js >> gauss.min.js');
 });
